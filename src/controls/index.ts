888aff8c1ef1577caeb7509a0390495d27a1cd19
shun cheung <shunkongcheung@tutanota.com>
2022-03-28 22:00:16 -0700

    Feture/first person control (#1)
    * feat(BaseControl): init
    
    * feat(FirstPersonControl): init
    
    * feat(getControl): init
    
    * feat(components): not exporting Heart
    
    * feat(index.ts): use control
    
    Co-authored-by: Shun Kong Cheung <shcheung@microsoft.com>

---
+++ src/controls/index.ts
@@ -0,0 +1,4 @@
+import { getControl } from "./getControl";
+import { FirstPersonControl } from "./FirstPersonControl";
+
+export { getControl, FirstPersonControl };
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,100 @@
 import * as THREE from "three";
-import { Heart, Line, RotateCube } from "./components";
+import * as Components from "./components";
+import { getControl, FirstPersonControl } from "./controls";
 
-const getSetup = () => {
-  const scene = new THREE.Scene();
+const getCamera = () => {
   const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
-    0.1,
+    1,
     1000
   );
-  camera.position.set(0, 0, 100);
-  camera.lookAt(0, 0, 0);
+  camera.position.set(-1, 2, 3);
 
-  const renderer = new THREE.WebGLRenderer();
-  const MARGIN = 20;
-  renderer.setSize(window.innerWidth - MARGIN, window.innerHeight - MARGIN);
-  document.body.appendChild(renderer.domElement);
-
-  return { camera, renderer, scene };
+  return camera;
 };
 
-window.addEventListener("load", () => {
-  const { camera, renderer, scene } = getSetup();
+const getRenderer = () => {
+  const renderer = new THREE.WebGLRenderer();
+  const MARGIN = 40;
+  renderer.setSize(window.innerWidth - MARGIN, window.innerHeight - MARGIN);
+  document.body.appendChild(renderer.domElement);
+  return renderer;
+};
 
-  const components = [new Heart(), new Line(), new RotateCube()];
+const handleWindowResize = (
+  camera: THREE.PerspectiveCamera,
+  renderer: THREE.WebGLRenderer
+) => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-  components.map((component) => scene.add(component.object));
+  renderer.setSize(window.innerWidth, window.innerHeight);
+};
 
-  function animate() {
-    requestAnimationFrame(animate);
-    components.map((component) => component.animate());
-    renderer.render(scene, camera);
-  }
-  animate();
-});
+const insertBackground = (scene: THREE.Scene) => {
+  scene.background = new THREE.Color(0xa0a0a0);
+  scene.fog = new THREE.Fog(0xa0a0a0, 10, 50);
+};
+
+const insertLighting = (scene: THREE.Scene) => {
+  const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444);
+  hemiLight.position.set(0, 20, 0);
+  scene.add(hemiLight);
+
+  const dirLight = new THREE.DirectionalLight(0xffffff);
+  dirLight.position.set(3, 10, 10);
+  dirLight.castShadow = true;
+  dirLight.shadow.camera.top = 2;
+  dirLight.shadow.camera.bottom = -2;
+  dirLight.shadow.camera.left = -2;
+  dirLight.shadow.camera.right = 2;
+  dirLight.shadow.camera.near = 0.1;
+  dirLight.shadow.camera.far = 40;
+  scene.add(dirLight);
+
+  const mesh = new THREE.Mesh(
+    new THREE.PlaneGeometry(100, 100),
+    new THREE.MeshPhongMaterial({ color: 0x999999, depthWrite: false })
+  );
+  mesh.rotation.x = -Math.PI / 2;
+  mesh.receiveShadow = true;
+  scene.add(mesh);
+};
+
+(() => {
+  const scene = new THREE.Scene();
+  const clock = new THREE.Clock();
+  const camera = getCamera();
+  const renderer = getRenderer();
+  const control = getControl(
+    new FirstPersonControl(),
+    camera,
+    renderer.domElement
+  );
+
+  window.addEventListener("load", () => {
+    insertBackground(scene);
+    insertLighting(scene);
+
+    const components = Object.values(Components).map(
+      (Component) => new Component()
+    );
+
+    components.map((component) => scene.add(component.object));
+
+    function animate() {
+      requestAnimationFrame(animate);
+      // components.map((component) => component.animate());
+      control.update(clock.getDelta());
+      renderer.render(scene, camera);
+    }
+    animate();
+  });
+
+  window.addEventListener("resize", () => {
+    handleWindowResize(camera, renderer);
+    control.handleResize();
+  });
+  control.handleResize();
+})();
