fcc0d506d215a0e515c6e692394584305ba33bbc
Shun Kong Cheung <shcheung@microsoft.com>
2022-03-28 21:56:52 -0700

    feat(components): not exporting Heart

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
-import { Heart } from "./Heart";
+// import { Heart } from "./Heart";
 import { Line } from "./Line";
 import { RotateCube } from "./RotateCube";
 
-export { Heart, Line, RotateCube };
+// export { Heart, Line, RotateCube };
+export { Line, RotateCube };
