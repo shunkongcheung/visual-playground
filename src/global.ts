77309085018185fe626382178884bb163599bab2
Shun Kong Cheung <shcheung@microsoft.com>
2022-04-03 01:39:04 -0700

    feat(primitives): init

---
+++ src/global.ts
@@ -0,0 +1,9 @@
+import "aframe";
+
+// Additional optional components
+import "aframe-animation-component";
+import "aframe-event-set-component";
+import "aframe-layout-component";
+import "aframe-template-component";
+
+import "./primitives";
