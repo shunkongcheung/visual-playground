8182e4e564c152c48ab5e887a9af500593ed42cb
Shun Kong Cheung <shcheung@microsoft.com>
2022-04-03 01:18:04 -0700

    feat(webpack.common.js): add global

--- webpack.common.js
+++ webpack.common.js
@@ -4,7 +4,10 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   mode: "production",
-  entry: path.resolve(__dirname, "./src/index.ts"),
+  entry: {
+    index: path.resolve(__dirname, "./src/index.ts"),
+    global: path.resolve(__dirname, "./src/global.ts"),
+  },
   module: {
     rules: [
       {
@@ -18,13 +21,15 @@ module.exports = {
     extensions: [".tsx", ".ts", ".js"],
   },
   output: {
-    filename: "bundle.[hash].js", // <- ensure unique bundle name
+    filename: "[name].[hash].js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
+      inject: "head",
+      chunks: ["global", "index"],
     }),
   ],
 };
